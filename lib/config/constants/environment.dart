d1e8f019f241e4c6ff83c04fa7dfc376bc772da9
Fabio Sánchez <fabiosanchez@MacBook-Pro-de-Fabio.local>
2025-01-28 16:26:54 -0500

    create custom appbar

--- lib/config/constants/environment.dart
+++ lib/config/constants/environment.dart
@@ -2,5 +2,5 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static String moviedbApiKey =
-      dotenv.env['MOVIEDB_API_KEY'] ?? 'No API Key found';
+      dotenv.env['THE_MOVIEDB_API_KEY'] ?? 'No API Key found';
 }
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cinema_pedia/presentation/providers/index.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../widgets/share/index.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -7,7 +11,41 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Placeholder(),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return Column(
+      children: [
+        const CustomAppbar(),
+        Expanded(
+          child: ListView.builder(
+              itemCount: nowPlayingMovies.length,
+              itemBuilder: (context, index) => ListTile(
+                    title: Text(nowPlayingMovies[index].title),
+                    subtitle: Text(nowPlayingMovies[index].overview),
+                  )),
+        )
+      ],
     );
   }
 }
